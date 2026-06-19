7bc70ff33e7e220d93054ab56c8c1ccc758b19da
hjosugi <h.jo.sugi@gmail.com>
2026-06-19 20:16:01 +0900

    refactor: remove public index files and encapsulate internal interfaces

--- src/config/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Public surface of the config layer.
- */
-
-export type { Config } from "./types"
-export { defaultConfig } from "./defaults"
-export { applySettings, normalizeSettings, settingsFromConfig } from "./settings"
-export type { UserSettings } from "./settings"
--- src/i18n/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Public surface of the i18n layer.
- */
-
-export type { Messages } from "./types"
-export { locales, supportedLocales, isLocaleTag, resolveLocale, getMessages } from "./catalog"
-export type { LocaleTag } from "./catalog"
